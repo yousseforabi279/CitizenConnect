61a87c0e5e6c22d9b64754cdfffb83bd380ff187
yousseforabi279 <yousseforabi279@gmail.com>
2026-08-27 08:36:10 +0300

    Deputyac

--- DeputyProject/Program.cs
+++ DeputyProject/Program.cs
@@ -18,11 +18,10 @@ var app = builder.Build();
 app.UseExceptionHandling();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
